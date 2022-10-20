864c8891cd795536748cecb0dce43f32e480cd91
kaiuri <uriel.acioli@gmail.com>
2022-10-20 12:39:46 -0300

    feat: missing bash captures

--- after/queries/bash/highlights.scm
+++ after/queries/bash/highlights.scm
@@ -3,9 +3,21 @@
 
 (subscript index: (word) @operator)
 
-[ "[[" "]]" "))" "((" ] @function.builtin
+[ "[[" "]]" 
+  "((" "))"] @function.builtin
 
 (string [ "\"" ] @string.delimiter)
 (test_operator ) @operator
 
 (file_redirect destination: (word) @constant (#any-of? @constant "/dev/null"))
+
+["|"   "|&"
+ "||"  "&&"
+ ">"   ">>"
+ "<"   "<<"
+ "<<-" "<<<"
+ "=="  "!="
+ ";&"  ";;&"
+ ";"   ";;"] @operator
+
+; ((word) @type (#lua-match? @type "^[-].*"))
