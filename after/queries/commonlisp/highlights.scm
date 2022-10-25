780017b38b988c5b9a24953da574969a2c335d44
kaiuri <uriel.acioli@gmail.com>
2022-10-25 12:54:11 -0300

    feat: improved commonlisp highlights

--- after/queries/commonlisp/highlights.scm
+++ after/queries/commonlisp/highlights.scm
@@ -1,3 +1,17 @@
 ; extends
 
 (str_lit ["\""] @string.delimiter)
+
+((sym_lit) @keyword.function
+ (#any-of? @keyword.function "let" "if"))
+
+((sym_lit) @operator
+ (#any-of? @operator "not"))
+
+((sym_lit) @function.builtin
+ (#any-of? @function.builtin
+  "error" "match-string"
+  "getenv" "string-match"
+  "expand"))
+
+
