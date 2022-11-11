bb349c6ca5188b0a794688a7b406dece71f359ca
kaiuri <uriel.acioli@gmail.com>
2022-11-11 17:10:27 -0300

    feat: improved lisp highlighs

--- after/queries/commonlisp/highlights.scm
+++ after/queries/commonlisp/highlights.scm
@@ -1,17 +1,31 @@
 ; extends
 
+; (list_lit
+;   .
+;   (sym_lit) @function.call
+;   (_))
+
 (str_lit ["\""] @string.delimiter)
 
-((sym_lit) @keyword.function
- (#any-of? @keyword.function "let" "if"))
-
+((sym_lit) @keyword
+ (#any-of? @keyword "let" "setq" "let*" "setf"))
+;
 ((sym_lit) @operator
- (#any-of? @operator "not"))
-
+ (#any-of? @operator "not" "or" "equal" "=" "+" "-" "/"))
+;
 ((sym_lit) @function.builtin
  (#any-of? @function.builtin
   "error" "match-string"
   "getenv" "string-match"
-  "expand"))
+  "setenv" "push"
+  "eval"
+  "dolist" "concat"
+  "expand" "cadr"))
 
+((sym_lit) @conditional
+ (#any-of? @conditional "if" "when-let" "when" "unless" "cond"))
 
+((sym_lit) @constant.builtin
+ (#any-of? @constant.builtin "t" "nil"))
+
+(kwd_lit) @field
