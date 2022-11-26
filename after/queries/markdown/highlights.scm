ad31636a3e3cbf800995706f5cdaa5b843501671
kaiuri <uriel.acioli@gmail.com>
2022-11-26 11:13:41 -0300

    fix: properly highlight markdown's checkboxes

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -4,10 +4,10 @@
 ; (setext_heading (paragraph) @text.title)
 
 [(atx_h1_marker)
- (atx_h2_marker) 
- (atx_h3_marker) 
- (atx_h4_marker) 
- (atx_h5_marker) 
+ (atx_h2_marker)
+ (atx_h3_marker)
+ (atx_h4_marker)
+ (atx_h5_marker)
  (atx_h6_marker)] @markdown.marker.h
 
 
@@ -17,5 +17,10 @@
  (list_marker_minus)
  (list_marker_star)
  (list_marker_dot)] @markdown.marker.list
- 
+
 (thematic_break) @markdown.marker.break
+
+[
+ (task_list_marker_checked)
+ (task_list_marker_unchecked)] @tag.delimiter
+
