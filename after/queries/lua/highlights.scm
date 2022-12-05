843a26cdace1e8d35c724d3304a016e36bef3b6c
kaiuri <uriel.acioli@gmail.com>
2022-12-05 14:41:51 -0300

    fix: lua queries
    Highlight `local` as `@storageclass`.

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -5,8 +5,7 @@
 (dot_index_expression
    table: (identifier) @namespace (#any-of? @namespace "vim"))
 
-; ((_
-;   name: (dot_index_expression
-;            (_)
-;            field: (identifier) @method.call)
-;   arguments: (arguments)))
+["local"] @storageclass
+
+(function_call
+  name: (identifier) @function.call)
