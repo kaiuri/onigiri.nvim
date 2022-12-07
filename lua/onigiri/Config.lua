bbb7a8b3ec0e771903b4e9928aa8468e2f6d8d5f
kaiuri <uriel.acioli@gmail.com>
2022-12-07 19:18:12 -0300

    chore(abandoned-order): 2022/12/07-19:18

--- lua/onigiri/Config.lua
+++ lua/onigiri/Config.lua
@@ -6,74 +6,69 @@ local keys = vim.tbl_keys
 ---@type fun(chunks: {text: string, hl_group: string}[], history?: boolean, opts: nil[])
 local nvim_echo = vim.api.nvim_echo
 
+---@enum DEFAULT_THEME
 local DEFAULT_THEME = {
-  ---@enum FBgColor
+  ---@enum Background
   Background = {
-    default = "#303841",
+    default  = "#303841",
     emphasis = "#2e353e",
-    muted = "#3b454e",
+    muted    = "#3b454e",
   },
-  ---@enum FColor
+  ---@enum Colors
   Colors = {
-    Accent = "#95b2d6",
-    Caution = "#f9ae58",
-    Danger = "#f97b58",
-    Error = "#ec5f66",
-    Hint = "#5fb4b4",
+    Accent    = "#95b2d6",
+    Caution   = "#f9ae58",
+    Danger    = "#f97b58",
+    Error     = "#ec5f66",
+    Hint      = "#5fb4b4",
     Important = "#fac761",
-    Info = "#99c794",
-    Note = "#5c99d6",
-    Trace = "#cc8ec6",
-    Warn = "#ee932b",
+    Info      = "#99c794",
+    Note      = "#5c99d6",
+    Trace     = "#cc8ec6",
+    Warn      = "#ee932b",
   },
-  ---@enum FFgColor
+  ---@enum Foreground
   Foreground = {
-    default = "#d8dee9",
+    default  = "#d8dee9",
     emphasis = "#f7f7f7",
-    muted = "#a6acb8",
-    surface = "#46525c",
+    muted    = "#a6acb8",
+    surface  = "#46525c",
   },
-  ---@enum FShadeColor
+  ---@enum Shade
   Shade = {
-    default = "#2f373f",
+    default  = "#2f373f",
     emphasis = "#2e363e",
   },
 }
--- table.sort(DEFAULT_THEME)
----@class FGroup table<K>: {[K]: string}
+
+---@class Theme
+---@field Foreground Foreground
+---@field Background Background
+---@field Shade      Shade
+---@field Colors     Colors
+---@field new        fun(theme?: Theme): Theme
 
 local function valid_hex(str)
   if not str then return false end
   return #str >= 4 and #str <= 7 and str:match("^#%x+$")
 end
 
----@param name "Foreground"|"Background"|"Shade"|"Colors"
----@return FGroup<`name`>
+---@generic T
+---@param name `T`
+---@return T
 local function FVar(name)
   local group = DEFAULT_THEME[name]
   local self  = setmetatable({}, {
     __index = group,
     __newindex = function(_, k, v)
-      if not valid_hex(v) then
-        error("Invalid hex color: " .. v)
-      end
-      if not contains(keys(group), k) then
-        error("Invalid color entry: " .. k)
-      end
-
+      assert(valid_hex(v), ("Invalid hex color: '%s'"):format(v))
+      assert(contains(keys(group), k), ("Invalid key entry: '%s'"):format(k))
       group[k] = v
     end,
   })
 
   return self
 end
-
----@class Theme
----@field Foreground FGroup<FFgColor>
----@field Background FGroup<FBgColor>
----@field Shade      FGroup<FShadeColor>
----@field Colors     FGroup<FColor>
----@field new fun(theme?: Theme): Theme
 
 ---@type Theme
 local Theme = {}
@@ -91,10 +86,11 @@ function Theme.new()
   return self
 end
 
+---@type Theme
 local theme = Theme.new()
 vim.pretty_print(theme)
 
-theme.Foreground.default = '#000'
+theme.Foreground.default = 'jflajf'
 
 vim.pretty_print(theme.Foreground.default)
 vim.pretty_print(theme)
